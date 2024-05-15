8230434bf4763334c873cefb4d49536009a67ac2
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2024-05-15 11:09:31 +0300

    webapp

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { TonConnectButton} from "@tonconnect/ui-react";
 import {useMainContract} from "./hooks/useMainContract.ts";
 import {useTonConnect} from "./hooks/useTonConnect.ts";
+import WebApp from '@twa-dev/sdk';
 
 function App() {
     const {
@@ -13,6 +14,10 @@ function App() {
 
     const { connected } = useTonConnect()
 
+    const showAlert = () => {
+        WebApp.showAlert("Hey there!");
+    };
+
     return (
         <div>
             <div>
@@ -20,16 +25,28 @@ function App() {
             </div>
             <div>
                 <div className='Card'>
+                    <b>{WebApp. platform}</b>
                     <b>Our contract Address</b>
                     <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
                     <b>Our contract Balance</b>
-                    <div className='Hint'>{contract_balance}</div>
+                       {contract_balance && (
+                        <div className= 'Hint'>{contract_balance}
+                    </div>
+                    )}
                 </div>
 
                 <div className='Card'>
                     <b>Counter Value</b>
                     <div>{counter_value ?? "Loading..."}</div>
                 </div>
+
+                <a
+                          onClick={() => {
+                            showAlert();
+                          }}
+                        >
+                          Show Alert
+                </a>
 
                 {connected && (
                     <a
